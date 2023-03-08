9c12839d97615f1bddbc1d30e52f4fe09f832a7c
nnydtmg <nnydtmg@gmail.com>
2023-03-08 21:34:06 +0900

    Update Using SecretsManager at app.py

--- lambda/app.py
+++ lambda/app.py
@@ -9,6 +9,7 @@ import os
 import logging
 import base64
 from botocore.exceptions import ClientError
+import ast
 
 TODAY = datetime.datetime.utcnow()
 FIRST_DAY_OF_THE_MONTH = TODAY - datetime.timedelta(days=TODAY.day - 1)
@@ -41,53 +42,36 @@ def get_total_cost():
     return total_cost
 
 def get_secret():
-  secret_name = "alertapp/url"
-  region_name = "ap-northeast-1"
 
-  # Create a Secrets Manager client
-  session = boto3.session.Session()
-  client = session.client(
-      service_name='secretsmanager',
-      region_name=region_name
-  )
-  try:
-      get_secret_value_response = client.get_secret_value(
-          SecretId=secret_name
-      )
-  except ClientError as e:
-      if e.response['Error']['Code'] == 'DecryptionFailureException':
-          # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-          # Deal with the exception here, and/or rethrow at your discretion.
-          raise e
-      elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-          # An error occurred on the server side.
-          # Deal with the exception here, and/or rethrow at your discretion.
-          raise e
-      elif e.response['Error']['Code'] == 'InvalidParameterException':
-          # You provided an invalid value for a parameter.
-          # Deal with the exception here, and/or rethrow at your discretion.
-          raise e
-      elif e.response['Error']['Code'] == 'InvalidRequestException':
-          # You provided a parameter value that is not valid for the current state of the resource.
-          # Deal with the exception here, and/or rethrow at your discretion.
-          raise e
-      elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-          # We can't find the resource that you asked for.
-          # Deal with the exception here, and/or rethrow at your discretion.
-          raise e
-  else:
-      # Decrypts secret using the associated KMS CMK.
-      # Depending on whether the secret is a string or binary, one of these fields will be populated.
-      if 'SecretString' in get_secret_value_response:
-          secret = get_secret_value_response['SecretString']
-      else:
-          decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+    secret_name = "alertapp/url"
+    region_name = "ap-northeast-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    secret = get_secret_value_response['SecretString']
+    return secret
 
 def handler(event, context):
   text = "ID:{} の {}までのAWS合計料金 : ${}".format(id_info['Account'], END_DATE, get_total_cost())
   content = {"text": text}
 
-  SLACK_POST_URL = get_secret()
+  secret = ast.literal_eval(get_secret())
+  SLACK_POST_URL = secret['costalert-slackapp-url']
 
   slack_message = {
       'channel': SLACK_CHANNEL,

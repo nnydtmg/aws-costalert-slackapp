3568b404000376092d49bc34e25ebb0841c0cea9
nnydtmg <nnydtmg@gmail.com>
2023-03-08 22:08:16 +0900

    Modify stack code

--- lib/aws-costalert-slackapp-stack.ts
+++ lib/aws-costalert-slackapp-stack.ts
@@ -5,14 +5,12 @@ import * as events from "aws-cdk-lib/aws-events";
 import * as targets from 'aws-cdk-lib/aws-events-targets';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { StringParameter } from 'aws-cdk-lib/aws-ssm';
-// import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 
 export class AwsCostalertSlackappStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Parameter
-    // const slackUrl = StringParameter.valueForStringParameter(this, '/costalert-slackapp/url');
     const slackChannel = StringParameter.valueForStringParameter(this, '/costalert-slackapp/channel');
 
     // lambda-layer
@@ -21,14 +19,30 @@ export class AwsCostalertSlackappStack extends cdk.Stack {
       compatibleRuntimes: [lambda.Runtime.PYTHON_3_9],
     });
     
+    const executionLambdaRole = new iam.Role(
+      this,
+      'CostalertLambdaExecutionRole',
+      {
+        roleName: 'costalert-lambda-execution-role',
+        assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName(
+            'service-role/AWSLambdaBasicExecutionRole'
+          ),
+          iam.ManagedPolicy.fromAwsManagedPolicyName(
+            'SecretsManagerReadWrite'
+          )
+        ],
+      }
+    );
+
     // lambda
-    const sampleLambda = new lambda.Function(this, 'NotifyPriceHandler', {
+    const costalertLambda = new lambda.Function(this, 'NotifyPriceHandler', {
       runtime: lambda.Runtime.PYTHON_3_9,    // execution environment
       code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
       handler: 'app.handler',                // file is "hello", function is "handler"
       environment: {
         TZ: 'Asia/Tokyo',
-        // SLACK_POST_URL: slackUrl,
         SLACK_CHANNEL: slackChannel,
       },
       layers: [layer],
@@ -36,14 +50,15 @@ export class AwsCostalertSlackappStack extends cdk.Stack {
         actions: ['ce:GetCostAndUsage'],
         resources: ['*'],
       })],
+      role: executionLambdaRole
     });
 
     // EventBridge
-    new events.Rule(this, "sampleRule", {
+    new events.Rule(this, "costalertRule", {
       // JST で毎日 AM9:10 に定期実行
       // 参考 https://docs.aws.amazon.com/ja_jp/AmazonCloudWatch/latest/events/ScheduledEvents.html#CronExpressions
       schedule: events.Schedule.cron({minute: "10", hour: "0"}),
-      targets: [new targets.LambdaFunction(sampleLambda, {retryAttempts: 3})],
-  });
+      targets: [new targets.LambdaFunction(costalertLambda, {retryAttempts: 3})],
+    });
   }
 }
